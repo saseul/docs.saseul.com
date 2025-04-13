c8c67cc16da123149d78de9d38e0b15f066ab034
ArtiFriends <jjal@artifriends.com>
2025-04-13 19:32:50 +0900

    fix url

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250413.192726';
+const cacheName = 'chirpy-20250413.193239';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
